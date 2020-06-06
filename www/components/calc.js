4674df3a48ad8e292d45afe306b69181907cc76c
samanthavictoria <sasavicsouza@gmail.com>
2020-06-07 06:42:29 +0900

    quase finalizado, faltam imagens e o css

--- www/components/calc.js
+++ www/components/calc.js
@@ -1,2 +1,28 @@
 // This is a JavaScript file
+$(document).on("click","#verificar", function(){
+var a= parseFloat($("#v1").val());
+var b= parseFloat($("#v2").val());
+var c= parseFloat($("#v3").val());
+
+if(a<(b+c) && b<(a+c) && c<(a+b)){
+  if(a==b && b==c){
+   $("#resposta").html("equilatero");
+  }
+
+  else if (a==b && b!=c || a==c && c!=b || b==c && c!=a){
+    $("#resposta").html("isoceles");
+  }
+
+  else{
+   $("#resposta").html("escaleno");
+  }
+}
+
+else{
+  navigator.notification.alert("Triângulo inválido",null,"Aviso","ok");
+  navigator.vibrate(1000);
+}
+
+});
+
 
